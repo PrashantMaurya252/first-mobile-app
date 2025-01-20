993a7f79810417a92d4585d45fa83b67d88336f0
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-01-20 22:36:22 +0530

    building sign in page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="text-red-700">Welcome To My First App</Text>
+      <Text className="text-red-700 font-rubik text-lg">Welcome To My First App</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
