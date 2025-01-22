45fd187d29eac0874cceda306668b80ac86b0eb4
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-01-23 00:01:42 +0530

    styling profile page

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,7 +1,25 @@
-import { View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, Image, TouchableOpacity, ImageSourcePropType } from 'react-native'
 import React from 'react'
 import icons from '@/constants/icons'
 import images from '@/constants/images'
+import { settings } from '@/constants/data';
+
+interface SettingsItemProps{
+  icon:ImageSourcePropType;
+  title:string;
+  onPress?:()=>void;
+  textStyle?:string;
+  showArrow?:boolean
+}
+const SettingsItem =({icon,title,onPress,textStyle,showArrow=true}:SettingsItemProps)=>(
+  <TouchableOpacity onPress={onPress} className='flex flex-row items-center justify-between py-3'>
+    <View className='flex flex-row items-center gap-3'>
+      <Image source={icon} className='size-6'/>
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>{title}</Text>
+    </View>
+    {showArrow && <Image source={icons.rightArrow} className='size-5'/>}
+  </TouchableOpacity>
+)
 
 const Profile = () => {
   const handleLogout = async()=>{}
@@ -15,11 +33,23 @@ const Profile = () => {
         <View className='flex-row justify-center flex mt-5'>
           <View className='flex flex-col items-center relative mt-5'>
             <Image source={images.avatar} className='size-44 relative rounded-full'/>
-            <TouchableOpacity className='absolute bottom-11 right-2'>
+            <TouchableOpacity className='absolute bottom-11 right-9'>
               <Image source={icons.edit} className='size-9'/>
             </TouchableOpacity>
             <Text className='text-2xl font-rubik-bold mt-2'>Adrian | JS Mastery</Text>
           </View>
+        </View>
+        <View className='flex flex-col mt-10'>
+          <SettingsItem icon={icons.calendar} title='My Bookings'/>
+          <SettingsItem icon={icons.wallet} title='Payments'/>
+        </View>
+        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'>
+          {settings.slice(2).map((item,index)=>(
+            <SettingsItem key={index} {...item}/>
+          ))}
+        </View>
+        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'>
+          <SettingsItem icon={icons.logout} title='Logout' textStyle='text-danger' showArrow={false} onPress={handleLogout}/>
         </View>
       </ScrollView>
     </SafeAreaView>
