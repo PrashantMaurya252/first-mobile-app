58913c2243b45b5e05cf1bc0d867853e605fa014
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-01-25 13:16:10 +0530

    features component built

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
@@ -10,7 +11,7 @@ export default function Index() {
 
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5">
+      <View className="px-5 ">
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row items-center">
             <Image source={images.avatar} className="size-12 rounded-full"/>
@@ -33,9 +34,23 @@ export default function Index() {
         <View className="flex flex-row gap-5 mt-5">
           <FeaturedCard/>
           <FeaturedCard/>
+          <FeaturedCard/>
         </View>
+
+        
       </View>
-      
+      <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+          </TouchableOpacity>
+        </View>
+        <Filters/>
+
+        <View className="flex flex-row gap-5 mt-5">
+          <Card/>
+          <Card/>
+        </View>
       </View>
 
       
